ed29eb7d734a6ed7f869d3452e3f00bab3411dcd
brycemillis <70339482+brycemillis@users.noreply.github.com>
2021-05-02 18:00:22 -0700

    GUI Prompts

---
+++ gui.cpp
@@ -0,0 +1,82 @@
+#include "gui.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
+using namespace std;
+
+
+const Gui::MainMenu::MENU_CHOICE Gui::MainMenu::getMenuChoice()
+{
+    cout << "Would you like to edit the metadata?" << endl;
+    cout << "0) Exit program"
+    cout << "1) Yes" << endl;
+    cout << "2) No"  << endl;
+    int choice;
+    cin >> choice;
+    return (Gui::MENU_CHOICE)choice;
+}
+
+int Gui::ProcessingMenu::getMenuChoice()
+{
+    //  display the menu
+    std::string input;
+    int menuChoices = 0;
+    bool shouldExitMenu = false;
+    while( !shouldExitMenu )
+    {
+        //  reset the flag, we are assuming everything will be ok
+        shouldExitMenu = true;
+        //  display the menu
+        cout << "Enter one or more choies, separated by a comma, or 0 to exit:" << endl;
+        cout << "1)  Normalisation" << endl;
+        cout << "2)  Noise Gating" << endl;
+        cout << "3)  Echo" << endl;
+        cout << "0)  Exit Program" << endl;
+        //  input the choices
+        cin >> input;
+        stringstream ss(input);
+        //  parse the choices
+        menuChoices = 0;
+        for (int i; ss >> i;) {
+            if( !i ) return Gui::ProcessingMenu::EXIT;
+            if( i < 0  ||  i > 3)
+            {
+                //  display error message and indicate we should not exit the menu
+                cout << "Unknown option " << i << endl;
+                shouldExitMenu = false;
+                break;
+            }
+            //  ignore comma
+            if (ss.peek() == ',')
+                ss.ignore();
+            //  bit-wise OR the value
+            menuChoices |= (1 << i);
+        }
+    }
+    //  return the user's choices
+    return menuChoices;    
+}
+
+
+const Gui::FileMenu::MENU_CHOICE Gui::FileMenu::getMenuChoice()
+{
+    cout << "Would you like to edit the processed file's name?" << endl;
+    cout << "0) Exit program" << endl;
+    cout << "1) Edit file name" << endl;
+    int choice;
+    cin >> choice;
+    return (Gui::MENU_CHOICE)choice;
+}
+
+const Gui::CSVmenu::MENU_CHOICE Gui::CSVmenu::getMenuChoice()
+{
+    cout << "Would you like to create a CSV file?" << endl; 
+    cout << "0) Exit program" << endl;
+    cout << "1) Create CSV file" << endl; 
+    int choice; 
+    cin >> choice;
+    return (Gui::MENU_CHOICE)choice;
+}
---
+++ gui.h
@@ -0,0 +1,51 @@
+#ifndef GUI_H
+#define GUI_H
+
+namespace Gui
+{
+    namespace MainMenu
+    {
+        
+    
+    enum MENU_CHOICE
+    {
+        EXIT = 0
+        EDIT_METADATA = 1
+       
+        
+    };
+    }
+    const MENU_CHOICE getMenuChoice();
+
+    namespace ProcessingMenu
+    {
+        const int UNKNOWN = -1;
+        const int EXIT = 0;
+        const int NORMALISATION = 2;
+        const int NOISE_GATING = 4;
+        const int ECHO = 8;
+        int getMenuChoice();
+    }
+    
+    namespace FileMenu
+    {
+        enum MENU_CHOICE
+        {
+            EXIT = 0
+            EDIT_FILE_NAME = 1
+        };
+    const MENU_CHOICE getMenuChoice();
+    }
+    
+    namespace CSVMenu
+    {
+        enum MENU_CHOICE
+        {
+            EXIT = 0
+            MAKE_CSV = 1
+        };
+        const MENU_CHOICE getMenuChoice();
+    }
+};
+
+#endif
